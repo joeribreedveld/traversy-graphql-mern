d2a2d6fadea2d712ae0b3bebbf8e923a2c0e68ae
Duck1s <32653@ma-web.nl>
2022-10-22 22:10:54 +0200

    feat(database): add database connection with mongoose

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const colors = require("colors");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const port = process.env.PORT || 5000;
+const connectDB = require("./config/db");
 const schema = require("./schema/schema");
 
 const app = express();
+
+// Connect to database
+connectDB();
 
 app.use(
   "/graphql",
