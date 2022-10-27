f7ed02b5d72336ec4f3b3c42c6ca8d611e6b1a1e
Duck1s <32653@ma-web.nl>
2022-10-27 15:45:02 +0200

    add last things, stopping with tutorial

---
+++ client/src/components/AddProjectModal.jsx
@@ -0,0 +1,136 @@
+import { useState } from "react";
+import { FaList } from "react-icons/fa";
+import { useMutation, useQuery } from "@apollo/client";
+import { ADD_PROJECT } from "../mutations/projectMutations";
+import { GET_PROJECTS } from "../queries/projectQueries";
+
+const AddProjectModal = () => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [clientId, setClientId] = useState("");
+  const [status, setStatus] = useState("new");
+
+  const [addProject] = useMutation(ADD_PROJECT, {
+    variables: {
+      name,
+      description,
+      clientId,
+      status,
+    },
+    update(cache, { data: { addProject } }) {
+      const { projects } = cache.readQuery({ query: GET_PROJECTS });
+      cache.writeQuery({
+        query: GET_PROJECTS,
+        data: { projects: projects.concat([addProject]) },
+      });
+    },
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (name === "" || description === "" || status === "") {
+      alert("Please fill in all fields");
+    }
+
+    addProject(name, description, clientId, status);
+
+    setName("");
+    setDescription("");
+    setClientId("");
+    setStatus("new");
+  };
+
+  return (
+    <>
+      <button
+        type="button"
+        className="btn btn-secondary"
+        data-bs-toggle="modal"
+        data-bs-target="#addProjectModal"
+      >
+        <div className="d-flex align-items-center">
+          <FaList className="icon" />
+          <div>Add Project</div>
+        </div>
+      </button>
+
+      <div
+        className="modal fade"
+        id="addProjectModal"
+        tabIndex="-1"
+        aria-labelledby="addProjectModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="addProjectModalLabel">
+                Add Project
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form onSubmit={onSubmit}>
+                <div className="mb-3">
+                  <label className="form-label" htmlFor="">
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label" htmlFor="">
+                    Description
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="description"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label" htmlFor="">
+                    Status
+                  </label>
+                  <select
+                    type="text"
+                    className="form-control"
+                    id="status"
+                    value={status}
+                    onChange={(e) => setStatus(e.target.value)}
+                  >
+                    <option value="new">Not Started</option>
+                    <option value="progress">In Progress</option>
+                    <option value="completed">Completed</option>
+                  </select>
+                </div>
+                <button
+                  type="submit"
+                  data-bs-dismiss="modal"
+                  className="btn btn-primary"
+                >
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AddProjectModal;
